f44fad1290457e79eff6bceaccb823c6c9c0a823
AgentRiver <38084339+AgentRiver@users.noreply.github.com>
2018-04-13 06:51:44 +0100

    Update discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -63,20 +63,20 @@ async def on_message(message):
         for x in client.get_all_emojis():
             if x.id == '367746698639835140':
                 return await client.add_reaction(message, x)
-##    if "Hello" in message.content:
-##        await client.send_message(message.author, "Hello!")
-##    if "hello" in message.content:
-##        await client.send_message(message.author, "Hello!")
-##    if "Blame" in message.content:
-##        blameUsers = []
-##        blames = ["being Satan", "being blamed for", "burning down a building", "whatever Leyham did", "deleting System32", "the sky falling", "...", "not voting for Donald Trump", "smashing a window", "eating McDonalds", "blowing up the moon", "the moon landing being fake", "Leyham losing his game! GRRRR", "stabbing people", "handing out knifes to kids", "being too happy", "speling this wroong", "crashing a bus", "playing Fortnite", "staying up past their bedtime", "being too small", "being too big", "writing looooooooooooooooooong words", "writing", "MURDER", "not murdering someone", "spreading juicy leaks", "eating juicy leaks", "making up fake juicy leaks", "being a meme", "not being as *cool* as freddie", "dumping plastic in the ocean", "making a water level", "enjoying sports", "making WATCHMOJO PROPAGANDA", "bloatware", "viruses", "pretending to listen", "mass producing babies", "being a screaming child", "dead", "paying for WinRAR", "still making We are Number One Memes", "being a dead meme", "liking memes that are already dead", "playing *Mario Tennis*", "putting mushrooms on pizza", "not having cheese in a cheese sandwich", "liking Tetris (please don't leave Leyham, it was a mistake)", "being a mistake"]
-##        x = message.server.members
-##        for member in x:
-##            #print(member.name)
-##            blameUsers.append(member)
-##        #print(blameUsers)
-##        #print(len(blames))
-##        await client.send_message(message.channel, blameUsers[ random.randint( 0,len(blameUsers)-1 ) ].mention + " is being blamed for " + blames[ random.randint( 0,len(blames)-1 ) ])
+    if "Hello" in message.content:
+        await client.send_message(message.author, "Hello!")
+    if "hello" in message.content:
+        await client.send_message(message.author, "Hello!")
+    if "Blame" in message.content:
+        blameUsers = []
+        blames = ["being Satan", "being blamed for", "burning down a building", "whatever Leyham did", "deleting System32", "the sky falling", "...", "not voting for Donald Trump", "smashing a window", "eating McDonalds", "blowing up the moon", "the moon landing being fake", "Leyham losing his game! GRRRR", "stabbing people", "handing out knifes to kids", "being too happy", "speling this wroong", "crashing a bus", "playing Fortnite", "staying up past their bedtime", "being too small", "being too big", "writing looooooooooooooooooong words", "writing", "MURDER", "not murdering someone", "spreading juicy leaks", "eating juicy leaks", "making up fake juicy leaks", "being a meme", "not being as *cool* as freddie", "dumping plastic in the ocean", "making a water level", "enjoying sports", "making WATCHMOJO PROPAGANDA", "bloatware", "viruses", "pretending to listen", "mass producing babies", "being a screaming child", "dead", "paying for WinRAR", "still making We are Number One Memes", "being a dead meme", "liking memes that are already dead", "playing *Mario Tennis*", "putting mushrooms on pizza", "not having cheese in a cheese sandwich", "liking Tetris (please don't leave Leyham, it was a mistake)", "being a mistake"]
+        x = message.server.members
+        for member in x:
+            #print(member.name)
+            blameUsers.append(member)
+        #print(blameUsers)
+        #print(len(blames))
+        await client.send_message(message.channel, blameUsers[ random.randint( 0,len(blameUsers)-1 ) ].mention + " is being blamed for " + blames[ random.randint( 0,len(blames)-1 ) ])
    
 ## 1 - Annoy Leyham!
 ##tetris_banned_words = ['tetris', 'tetriS', 'tetrIs', 'tetrIS', 'tetRis', 'tetRiS', 'tetRIs', 'tetRIS', 'teTris', 'teTriS', 'teTrIs', 'teTrIS', 'teTRis', 'teTRiS', 'teTRIs', 'teTRIS', 'tEtris', 'tEtriS', 'tEtrIs', 'tEtrIS', 'tEtRis', 'tEtRiS', 'tEtRIs', 'tEtRIS', 'tETris', 'tETriS', 'tETrIs', 'tETrIS', 'tETRis', 'tETRiS', 'tETRIs', 'tETRIS', 'Tetris', 'TetriS', 'TetrIs', 'TetrIS', 'TetRis', 'TetRiS', 'TetRIs', 'TetRIS', 'TeTris', 'TeTriS', 'TeTrIs', 'TeTrIS', 'TeTRis', 'TeTRiS', 'TeTRIs', 'TeTRIS', 'TEtris', 'TEtriS', 'TEtrIs', 'TEtrIS', 'TEtRis', 'TEtRiS', 'TEtRIs', 'TEtRIS', 'TETris', 'TETriS', 'TETrIs', 'TETrIS', 'TETRis', 'TETRiS', 'TETRIs', 'TETRIS']
@@ -96,6 +96,8 @@ async def on_message(message):
 ## if random == 1:
 ##    await client.kick(message.author)
 
+
+
 @client.event
 async def on_ready():
     print('Logged in as')
@@ -104,7 +106,3 @@ async def on_ready():
     print('------')
 
 client.run(TOKEN)
-
-#1 ROCK
-#2 SCISSORS
-#3 PAPER
